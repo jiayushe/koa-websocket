0769c8cb39be02eafd45e7ad3a1bbf46f64929be
zephyr <shejiayu1998@qq.com>
2019-07-12 01:18:39 +0800

    Fix incompatibility with sharedb-ace-backend

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 const url = require('url');
-const https = require('https');
 const compose = require('koa-compose');
 const co = require('co');
 const ws = require('ws');
@@ -7,14 +6,21 @@ const ws = require('ws');
 const WebSocketServer = ws.Server;
 const debug = require('debug')('koa:websockets');
 
-function KoaWebSocketServer(app) {
+function KoaWebSocketServer(app, options) {
   this.app = app;
   this.middleware = [];
+  this.options = options;
 }
 
-KoaWebSocketServer.prototype.listen = function listen(options) {
-  this.server = new WebSocketServer(options);
-  this.server.on('connection', this.onConnection.bind(this));
+KoaWebSocketServer.prototype.listen = function listen(server) {
+  const self = this;
+  this.server = new WebSocketServer({
+    server,
+  });
+  this.server.on('connection', (socket, req) => {
+    (self.onConnection.bind(self))(socket, req);
+    self.options.onConnection(socket);
+  });
 };
 
 KoaWebSocketServer.prototype.onConnection = function onConnection(socket, req) {
@@ -38,27 +44,14 @@ KoaWebSocketServer.prototype.use = function use(fn) {
   return this;
 };
 
-module.exports = function middleware(app, wsOptions, httpsOptions) {
+module.exports = function middleware(app, options) {
   const oldListen = app.listen;
   app.listen = function listen(...args) {
     debug('Attaching server...');
-    if (typeof httpsOptions === 'object') {
-      const httpsServer = https.createServer(httpsOptions, app.callback());
-      app.server = httpsServer.listen(...args);
-    } else {
-      app.server = oldListen.apply(app, args);
-    }
-    const options = { server: app.server };
-    if (wsOptions) {
-      Object.keys(wsOptions).forEach((key) => {
-        if (Object.prototype.hasOwnProperty.call(wsOptions, key)) {
-          options[key] = wsOptions[key];
-        }
-      });
-    }
-    app.ws.listen(options);
+    app.server = oldListen.apply(app, args);
+    app.ws.listen(app.server);
     return app.server;
   };
-  app.ws = new KoaWebSocketServer(app);
+  app.ws = new KoaWebSocketServer(app, options);
   return app;
 };
